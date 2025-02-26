6c05d527b799dabd7cffd27ca0baef08b8a7dd76
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-02-26 17:15:38 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250226/합과 평균의 차/sub-of-average-and-sum.cpp
+++ 250226/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    cout << a + b + c << endl << (a+b+c)/3 << (a+b+c)*2;
+    cout << a + b + c << endl << (a+b+c)/3 << endl << (a+b+c)*2;
     return 0;
 }
