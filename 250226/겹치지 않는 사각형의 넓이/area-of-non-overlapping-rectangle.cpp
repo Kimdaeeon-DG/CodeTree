23fd859ce6fe16aabf417544a5aa66b164354439
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-02-26 22:19:50 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250226/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int x1, y1, x2, y2;
+int arr[2010][2010] = {0, };
+int answer = 0;
+
+void setArea(int n) {
+    for (int i = y1; i < y2; i++) {
+        for (int j = x1; j < x2; j++) {
+            arr[i][j] = n;
+        }
+    }
+}
+
+int main() {
+    for (int k = 0; k < 2; k++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        setArea(1);
+    }
+    cin >> x1 >> y1 >> x2 >> y2;
+    setArea(0);
+
+    for (int i = 0; i < 2010; i++) {
+        for (int j = 0; j < 2010; j ++) {
+            if (arr[i][j] == 1) {
+                answer++;
+            }
+        }
+    } 
+
+    cout << answer;
+    return 0;
+}
