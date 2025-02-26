82675af70658b4d5709592c027ccdaa667a7f0c6
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-02-26 22:36:30 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 22ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 250226/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -6,6 +6,11 @@ int arr[2010][2010] = {0, };
 int answer = 0;
 
 void setArea(int n) {
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += 1000;
+    y1 += 1000;
+    x2 += 1000;
+    y2 += 1000;
     for (int i = y1; i < y2; i++) {
         for (int j = x1; j < x2; j++) {
             arr[i][j] = n;
@@ -14,18 +19,13 @@ void setArea(int n) {
 }
 
 int main() {
-    for (int k = 0; k < 2; k++) {
-        cin >> x1 >> y1 >> x2 >> y2;
-        setArea(1);
-    }
-    cin >> x1 >> y1 >> x2 >> y2;
+    setArea(1);
+    setArea(1);
     setArea(0);
 
-    for (int i = 0; i < 2000; i++) {
-        for (int j = 0; j < 2000; j ++) {
-            if (arr[i][j] == 1) {
-                answer++;
-            }
+    for (int i = 0; i < 2010; i++) {
+        for (int j = 0; j < 2010; j ++) {
+            if (arr[i][j]) answer++;
         }
     } 
 
