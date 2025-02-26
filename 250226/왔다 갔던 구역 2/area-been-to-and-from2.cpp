472fe6b187fe1b0bfb80578d2ba066224694f241
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-02-26 23:39:47 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250226/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int N, x, direction = 1, locate = 1000, answer = 0;
+char c;
+int arr[2010] = {0 ,};
+
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> x >> c;
+        if (c == 'L') direction = -1;
+        else direction = 1;
+        for (int j = 0; j < x; j++) {
+            arr[locate]++;
+            locate += direction;
+        }
+    }
+
+    for (int i = 0; i < 2010; i++) {
+        if (arr[i] > 1) {
+            answer++;
+        }
+    }
+
+    cout << answer;
+    return 0;
+}
