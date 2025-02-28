928cd78938faf6e59f6c64180397ad5b0cc1cce3
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-02-28 20:58:39 +0900

    Title: 1이 3개 이상 있는 위치, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250228/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[100][100];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+int dir_num, answer;
+int nx, ny, count;
+
+bool InRange(int x, int y) {
+    return (0 <= x && x < 5 && 0 <= y && y < 5);
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            count = 0;
+            for (int k = 0; k < 4; k++) {
+                nx = i + dy[k];
+                ny = j + dx[k];
+                if (InRange(nx, ny)) {
+                    if (grid[ny][nx]) count++;
+                }
+                
+            }
+            if (count > 2) {
+                answer++;
+            }
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
