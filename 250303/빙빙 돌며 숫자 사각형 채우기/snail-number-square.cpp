31a0718e3c7f97d7ac3af742a4c2bb2450131003
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-03-03 17:12:52 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250303/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int r, c;
+int a[100][100];
+
+int main() {
+    cin >> n >> r >> c;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
