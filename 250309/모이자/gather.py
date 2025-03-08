27d81170e758d342f00e2d3c28f9e4971ee241be
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-03-09 01:22:23 +0900

    Title: 모이자, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/모이자/gather.py
+++ 250309/모이자/gather.py
@@ -1,14 +1,15 @@
+import sys
 n = int(input())
 A = list(map(int, input().split()))
 
 # 한 점을 기준으로 abs(A[i] - A[j])의 총합이 적은 것
-answer = 0
+answer = sys.maxsize
 # O(n) + O(n-1)
 for i in range(n):
     count = 0
     for j in range(n):
-        count += abs(A[i] - A[j])
-    answer = max(answer, count)
+        count += abs(i - j) * A[j]
+    answer = min(answer, count)
 
 print(answer)
 
