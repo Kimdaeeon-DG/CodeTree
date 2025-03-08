3fe809ee9662e284e492819ee31ef2c44bcc89ee
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-03-09 01:24:26 +0900

    Title: 모이자, Time: 47ms, Memory: 17MB, Status: Passed - CodeTree

--- 250309/모이자/gather.py
+++ 250309/모이자/gather.py
@@ -2,9 +2,9 @@ import sys
 n = int(input())
 A = list(map(int, input().split()))
 
-# 한 점을 기준으로 abs(A[i] - A[j])의 총합이 적은 것
+# 한 점을 기준으로 abs(i-j) * A[j]의 총합이 적은 것
 answer = sys.maxsize
-# O(n) + O(n-1)
+# O(n) O(n) = O(n^2) -> 연산의 최대가 10000이니까 가능
 for i in range(n):
     count = 0
     for j in range(n):
