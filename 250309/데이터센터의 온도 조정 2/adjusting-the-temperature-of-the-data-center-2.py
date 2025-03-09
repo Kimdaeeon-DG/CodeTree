9a472a21ccc76fb8f0752c009e4434b39cc11c65
Kimdaeeon-DG <158052376+Kimdaeeon-DG@users.noreply.github.com>
2025-03-09 14:34:28 +0900

    Title: 데이터센터의 온도 조정 2, Time: 60ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250309/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,18 @@
+N, C, G, H = map(int, input().split())
+ranges = [tuple(map(int, input().split())) for _ in range(N)]
+
+answer = 0
+# 모든 상황 온도가 0 ~ 1000 경우에 대해 
+for i in range(0, 1001):
+    # 각 기기들의 작업량을 구함
+    count = 0
+    # 할 거 한다, 최대 작업량을 구함.
+    for j in range(N):
+        if ranges[j][0] <= i <= ranges[j][1]:
+            count += G
+        elif ranges[j][0] < i:
+            count += C
+        else:
+            count += H
+    answer = max(answer, count)
+print(answer)
